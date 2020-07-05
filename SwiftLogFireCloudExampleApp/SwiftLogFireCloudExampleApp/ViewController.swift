f55e331806a79e6211543c2b2ab2e9fca814b7f8
Tim Wise <tim@leisurehound.com>
2020-07-05 16:37:24 -0700

    Added an example app for demonstrating use of the SwiftLogFireCloud.  Its an empty app at this point.

---
+++ SwiftLogFireCloudExampleApp/SwiftLogFireCloudExampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLogFireCloudExampleApp
+//
+//  Created by Timothy Wise on 7/5/20.
+//  Copyright © 2020 Leisure Hound Sports, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
