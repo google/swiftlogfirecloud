3b156b41ee38475ed6ab3220365ca2194fbb8475
Tim Wise <tim@leisurehound.com>
2020-07-27 23:22:06 -0700

    Update the sample app to use the new interface for client uploader.

--- SwiftLogFireCloudExampleApp/SwiftLogFireCloudExampleApp/CloudFileUploader.swift
+++ SwiftLogFireCloudExampleApp/SwiftLogFireCloudExampleApp/CloudFileUploader.swift
@@ -26,27 +26,21 @@ final class SwiftLogFireCloudUploader : CloudFileUploaderProtocol {
     self.storage = storage
   }
   
-  func uploadFile(_ cloudManager: CloudLogFileManagerClientProtocol, from localFile: LocalLogFile, to cloudPath: String) {
+  func uploadFile(from localFile: LocalLogFile, to cloudPath: String, completion: @escaping (Result<LocalLogFile, CloudUploadError>) -> Void) {
 
     let storageReference = self.storage.reference()
     let cloudReference = storageReference.child(cloudPath)
     let uploadTask = cloudReference.putFile(from: localFile.fileURL, metadata: nil) { metadata, error in
       if let error = error {
         print(error.localizedDescription)
-        // Add the file to end of the queue upon error.  If its a rights error, it will rety ad infinitum
-        cloudManager.reportUploadStatus(.failure(CloudUploadError.failedToUpload(localFile)))
-        
-        // handle the error, not sure how to cascade the error here since client never knows when log to cloud is
-        // invoked, as the logging is fire and forget on behalf of the client.  Perhaps add Crashlytics non-fatal
-        // error logging for client to monitor incident rates.
+        completion(.failure(CloudUploadError.failedToUpload(localFile)))
       }
     }
     _ = uploadTask.observe(.success) { snapshot in
-      cloudManager.reportUploadStatus(.success(localFile))
+      completion(.success(localFile))
     }
     _ = uploadTask.observe(.failure) { snapshot in
-      cloudManager.reportUploadStatus(.failure(CloudUploadError.failedToUpload(localFile)))
+      completion(.failure(CloudUploadError.failedToUpload(localFile)))
     }
   }
-  
 }
