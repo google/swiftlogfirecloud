d3aaae99ea0785036290bebadc6c82979c714368
Tim Wise <tim@leisurehound.com>
2021-06-05 12:08:10 -0700

    Revert "Merge branch 'master' into macosSupport-issue2"
    This reverts commit 28640572210c8252b64c4ece0af3ee621d4baa6f, reversing
    changes made to 6699d90393d2195c150b1962b6a66f59ce29b558.

--- Sources/SwiftLogFireCloud/SwiftLogManager.swift
+++ Sources/SwiftLogFireCloud/SwiftLogManager.swift
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import Foundation
 #if canImport(UIKit)
   import UIKit
 #endif
@@ -36,7 +37,10 @@ internal class SwiftLogManager {
   internal var successiveWriteFailures: Int = 0
   private let strandedFilesDelay: TimeInterval
   private var impairedMessages: Data?
+  
+  #if os(iOS)
   private var backgroundTaskID = UIBackgroundTaskIdentifier.invalid
+  #endif
   
   private func startWriteTimer(interval: TimeInterval) -> Timer {
     return Timer.scheduledTimer(
@@ -59,7 +63,7 @@ internal class SwiftLogManager {
     self.strandedFilesDelay = !config.isTesting ? 15 : 5
 
     writeTimer = startWriteTimer(interval: config.localFileBufferWriteInterval)
-
+    #if os(iOS)
     let notificationCenter = NotificationCenter.default
     notificationCenter.addObserver(
       self, selector: #selector(appWillResignActive),
@@ -67,7 +71,7 @@ internal class SwiftLogManager {
     notificationCenter.addObserver(
       self, selector: #selector(appWillResumeActive),
       name: UIApplication.willEnterForegroundNotification, object: nil)
-
+    #endif
     //wait 15s after startup, then attempt to push any files from previous runs up to cloud
     DispatchQueue.main.asyncAfter(deadline: .now() + strandedFilesDelay) {
       self.processStrandedFilesAtStartup()
@@ -84,6 +88,7 @@ internal class SwiftLogManager {
       writeTimer = startWriteTimer(interval: config.localFileBufferWriteInterval)
     }
   }
+  #if os(iOS)
   @objc internal func appWillResignActive(_ application: UIApplication) { //}, _ completionForTesting: (() -> Void)? = nil) {
     let backgroundEntitlementStatus = UIApplication.shared.backgroundRefreshStatus
     switch backgroundEntitlementStatus {
@@ -116,6 +121,7 @@ internal class SwiftLogManager {
 
     self.writeTimer?.invalidate()
   }
+  #endif
   
   private func getLocalTempDirectory() -> URL {
     #if os(iOS)
